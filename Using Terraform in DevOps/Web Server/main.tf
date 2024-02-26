ae088f2d7caabeb3094ed070be56cb3ad19144c9
djkone <djkone2025@gmail.com>
2024-02-25 20:28:00 -0600

    Update main.tf

--- Using Terraform in DevOps/Web Server/main.tf
+++ Using Terraform in DevOps/Web Server/main.tf
@@ -1,6 +1,6 @@
 
 
-resource "aws_instance" "ec2" {
+resource "aws_instance" "db" {
     ami = "ami-0440d3b780d96b29d"
     instance_type = "t2.micro"  
 
@@ -9,11 +9,25 @@ resource "aws_instance" "ec2" {
     } 
 }
 
-resource "aws_instance" "ec2" {
+resource "aws_instance" "web" {
     ami = "ami-0440d3b780d96b29d"
     instance_type = "t2.micro" 
     
     tags = {
         Name = "Web Server"
     } 
+}
+
+resource "aws_eip" "web_ip" {
+    instance = aws_instance.web.id
+}
+
+variable "ingress" {
+    type = list(number)
+    default = [ 80, 443 ]
+}
+
+variable "egress" {
+    type = list(number)
+    default = [ 80, 443 ]
 }
