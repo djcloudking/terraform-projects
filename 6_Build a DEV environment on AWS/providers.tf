bca7d0f3e478658f2d3679f12df48900dfe299ef
djkone <djkone2025@gmail.com>
2024-07-07 18:56:00 -0500

    Update providers.tf

--- 6_Build a DEV environment on AWS/providers.tf
+++ 6_Build a DEV environment on AWS/providers.tf
@@ -6,8 +6,10 @@ terraform {
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  region      = "us-east-1"
-  shared_credentials_files = "C:/Users/djkon/.aws/credentials"
+  region = "us-east-1"
+  shared_config_files      = ["/Users/tf_user/.aws/config"]
+  shared_credentials_files = ["/Users/tf_user/.aws/credentials"]
   profile                  = "djdev"
 }
