237a080a45700949bcdf999e1b04c05013d0d6d9
djkone <djkone2025@gmail.com>
2024-07-07 20:22:55 -0500

    Update main.tf

--- 6_Build a DEV environment on AWS/main.tf
+++ 6_Build a DEV environment on AWS/main.tf
@@ -7,3 +7,14 @@ resource "aws_vpc" "dj_vpc" {
     Name = "dev"
   }
 }
+
+resource "aws_subnet" "dj_public_subnet" {
+  vpc_id     = aws_vpc.dj_vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1"
+
+  tags = {
+    Name = "dev-public"
+  }
+}
