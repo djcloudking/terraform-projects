a9db16d3ddf50d3e3155744a37e4653c0f44dfd1
djkone <djkone2025@gmail.com>
2024-10-01 10:01:40 -0500

    Create NAT Gateway

--- 9_Deploy e-comerce website on AWS/natgateway.tf
+++ 9_Deploy e-comerce website on AWS/natgateway.tf
@@ -59,13 +59,13 @@ resource "aws_route_table" "private_route_table_az1" {
 # Associate private app subnet az1 to private route table az1
 resource "aws_route_table_association" "private_app_subnet_az1_route_table_az1_association" {
     subnet_id = aws_subnet.private_app_subnet_az1.id
-    route_table_id = aws_route_table.private_route_table.id
+    route_table_id = aws_route_table.private_route_table_az1.id
 }
 
 # Associate private data subnet az1 to private route table az1
 resource "aws_route_table_association" "private_data_subnet_az1_route_table_az1_association" {
     subnet_id = aws_subnet.private_app_subnet_az1.id
-    route_table_id = aws_route_table.private_route_table.id
+    route_table_id = aws_route_table.private_route_table_az1.id 
 }
 
 # Create private route table az2 and add route through nat gateway Az2
@@ -85,11 +85,11 @@ resource "aws_route_table_association" "private_data_subnet_az1_route_table_az1_
 # Associate private app subnet az2 to private route table az2
 resource "aws_route_table_association" "private_app_subnet_az2_route_table_az2_association" {
     subnet_id = aws_subnet.private_app_subnet_az2.id
-    route_table_id = aws_route_table.private_route_table.id
+    route_table_id = aws_route_table.private_app_route_table_az2.id 
 }
 
 # Associate private data subnet az2 to private route table az2
 resource "aws_route_table_association" "private_data_subnet_az2_route_table_az2_association" {
     subnet_id = aws_subnet.private_data_subnet_az2.id
-    route_table_id = aws_route_table.private_route_table_az2.id
+    route_table_id = aws_route_table.private_app_route_table_az2.id 
 }
