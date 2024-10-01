f4e93fc16c683d6e4189a561a8c2010c640decba
djkone <djkone2025@gmail.com>
2024-10-01 11:25:35 -0500

    Update security-group.tf

--- 9_Deploy e-comerce website on AWS/security-group.tf
+++ 9_Deploy e-comerce website on AWS/security-group.tf
@@ -1,1 +1,133 @@
-# Create security group
+# Create security groups
+
+# Create a security group for the application load balancer
+resource "aws_security_group" "alb_sg" {
+  name        = "alb security group"
+  description = "enable http/https access on port 80/443"
+  vpc_id      = aws_vpc.djvpc.id
+  
+  ingress {
+    description = "http access"
+    from_port         = 80
+    to_port           = 80
+    protocol          = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "https access"
+    from_port         = 443
+    to_port           = 443
+    protocol          = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = -1
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb_security_group"
+  }
+}
+
+
+# Create a security group for bastion host aka jump box
+resource "aws_security_group" "ssh_sg" {
+  name        = "ssh security group"
+  description = "enable ssh access on port 22"
+  vpc_id      = aws_vpc.djvpc.id
+  
+  ingress {
+    description = "ssh access"
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    cidr_blocks       = [var.ssh_location]
+  }
+  
+  egress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = -1
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh_security_group"
+  }
+}
+
+
+# Create a security group for the web server
+resource "aws_security_group" "webserver_security_group" {
+  name        = "ssh security group"
+  description = "enable http/https access on port 80/443 via alb sg and access on port 22 via ssh sg"
+  vpc_id      = aws_vpc.djvpc.id
+  
+  ingress {
+    description = "http access"
+    from_port         = 80
+    to_port           = 80
+    protocol          = "tcp"
+    security_groups   = [aws_security_group.alb_sg.id]
+  }
+
+  ingress {
+    description = "https access"
+    from_port         = 443
+    to_port           = 443
+    protocol          = "tcp"
+    security_groups   = [aws_security_group.alb_sg.id]
+  }
+  
+  ingress {
+    description = "ssh access"
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    security_groups = [aws_security_group.ssh_sg.id]
+  }
+  
+  egress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = -1
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "webserver security group"
+  }
+}
+
+
+# Create a security group for database
+resource "aws_security_group" "database_sg" {
+  name        = "database security group"
+  description = "enable mysql/aurora access on port 3306"
+  vpc_id      = aws_vpc.djvpc.id
+  
+  ingress {
+    description = "smysql/aurora access"
+    from_port         = 3306
+    to_port           = 3306
+    protocol          = "tcp"
+    security_groups   = [aws_security_group.webserver_security_group.id]
+  }
+  
+  egress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = -1
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "database_security_group"
+  }
+}
+
