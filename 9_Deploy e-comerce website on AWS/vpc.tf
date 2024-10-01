b9849945fa8c7346772c59687016af9be454355a
djkone <djkone2025@gmail.com>
2024-10-01 00:19:03 -0500

    Update vpc.tf

--- 9_Deploy e-comerce website on AWS/vpc.tf
+++ 9_Deploy e-comerce website on AWS/vpc.tf
@@ -1,1 +1,11 @@
 # create  VPC
+
+resource "aws_vpc" "djvpc" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "dev vpc"
+  }
+}
