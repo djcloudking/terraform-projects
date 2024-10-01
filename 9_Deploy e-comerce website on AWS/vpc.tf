88b9f458f7fdc9b3171a16ce0d146d86c5721749
djkone <djkone2025@gmail.com>
2024-10-01 02:16:42 -0500

    Update vpc.tf

--- 9_Deploy e-comerce website on AWS/vpc.tf
+++ 9_Deploy e-comerce website on AWS/vpc.tf
@@ -36,7 +36,7 @@ resource "aws_subnet" "public_subnet_az2" {
     vpc_id = aws_vpc.djvpc.id
 
     cidr_block = var.public_subnet_az2_cidr
-    availability_zone = "us-east-2a"
+    availability_zone = "us-east-1b"
     map_public_ip_on_launch = true 
 
     tags = {
@@ -58,8 +58,66 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
-# Associate public subnet az 1 to public route table 
+# Associate public subnet az1 to public route table 
 resource "aws_route_table_association" "public_subnet_az1_route_table_association" {
     subnet_id = aws_subnet.public_subnet_az1.id
     route_table_id = aws_route_table.public_route_table.id
+}
+
+# Associate public subnet az2 to public route table 
+resource "aws_route_table_association" "public_subnet_az2_route_table_association" {
+    subnet_id = aws_subnet.public_subnet_az2.id
+    route_table_id = aws_route_table.public_route_table.id
+}
+
+# Create Private app Subnet az1
+resource "aws_subnet" "private_app_subnet_az1" {
+    vpc_id = aws_vpc.djvpc.id
+
+    cidr_block = var.private_subnet_az1_cidr
+    availability_zone = "us-east-1c"
+    map_public_ip_on_launch = false 
+
+    tags = {
+        Name = "private app subnet az1"
+    }  
+}
+
+# Create PPrivate app Subnet az2
+resource "aws_subnet" "private_app_subnet_az2" {
+    vpc_id = aws_vpc.djvpc.id
+
+    cidr_block = var.private_subnet_az2_cidr
+    availability_zone = "us-east-1d"
+    map_public_ip_on_launch = false 
+
+    tags = {
+        Name = "private app subnet az2"
+    }   
+}
+
+# Create Private data Subnet az1
+resource "aws_subnet" "private_data_subnet_az1" {
+    vpc_id = aws_vpc.djvpc.id
+
+    cidr_block = var.private_data_subnet_az1_cidr
+    availability_zone = "us-east-1e"
+    map_public_ip_on_launch = false 
+
+    tags = {
+        Name = "private app subnet az1"
+    }  
+}
+
+# Create Private data Subnet az2
+resource "aws_subnet" "private_data_subnet_az2" {
+    vpc_id = aws_vpc.djvpc.id
+
+    cidr_block = var.private_data_subnet_az2_cidr
+    availability_zone = "us-east-1f"
+    map_public_ip_on_launch = false 
+
+    tags = {
+        Name = "private data subnet az2"
+    }   
 }
