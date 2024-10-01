b8806a1b32d9474b69bb8f214ac01d04ac9b7d3b
djkone <djkone2025@gmail.com>
2024-10-01 00:37:15 -0500

    Update variable.tf

--- 9_Deploy e-comerce website on AWS/variable.tf
+++ 9_Deploy e-comerce website on AWS/variable.tf
@@ -6,3 +6,17 @@ variable "vpc_cidr" {
     description = "vpc cidr block"
     type = string  
 }
+
+# create public subnet variables az1
+variable "public_subnet_az1_cidr" {
+    default = "10.0.0.0/24"
+    description = "public subnet az1 cidr block"
+    type = string
+}
+
+# create public subnet variables az2
+variable "public_subnet_az1_cidr" {
+    default = "10.0.0.1/24"
+    description = "public subnet az2 cidr block"
+    type = string
+}
