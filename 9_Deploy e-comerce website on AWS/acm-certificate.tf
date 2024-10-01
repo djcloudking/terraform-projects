96180d6c60186af791a58942c1c2a73d8114279c
djkone <djkone2025@gmail.com>
2024-10-01 14:51:49 -0500

    Delete acm-certificate.tf

--- 9_Deploy e-comerce website on AWS/acm-certificate.tf
+++
@@ -1,9 +0,0 @@
-# Create ACM certificate
-resource "aws_acm_certificate" "ssl_certificate" {
-  domain_name       = "www.talybo.com"
-  validation_method = "DNS"
-
-  tags = {
-    Name = "acm-cert"
-  }
-}
