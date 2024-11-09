6f5dc6278f6161f61067d928e0ddf636415afa4d
djkone <djkone2025@gmail.com>
2024-11-08 23:49:30 -0600

    Create a provider

---
+++ 11_Create a EKS Cluster using Terraform Modules/provider.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.10.1"
+    }
+  }
+
+  required_version = ">= 1.0, < 2.0"
+}
