79560023367901b390e0a88222eb80ae004e0c75
djkone <djkone2025@gmail.com>
2024-12-12 15:50:37 -0600

    Update main.tf

--- 15_Terraform workflow with Github Actions Pipeline/main.tf
+++ 15_Terraform workflow with Github Actions Pipeline/main.tf
@@ -4,19 +4,7 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
-  }
-
-# Uncomment everything above if you want a remote state backend
-  # Backend Configuration (Optional for Remote State)
-  # backend "s3" {
-  #  bucket         = "my-terraform-state-bucket"
-  #  key            = "terraform/workflow/terraform.tfstate"
-  # region         = "us-east-1"
-  #  dynamodb_table = "terraform-lock-table"
-  #  encrypt        = true
-  #}
-#}
-
+}
 
 provider "aws" {
   region = var.region
@@ -30,3 +18,5 @@ resource "aws_instance" "dj_instance" {
     Name = "DjInstance"
   }
 }
+}
+
