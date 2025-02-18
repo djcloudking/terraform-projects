026a03d9fbad14ca7caeeefcdd4d52fc62bd5025
djkone <djkone2025@gmail.com>
2025-02-18 01:42:58 -0600

    main.tf

---
+++ 19_Create a Spotify playlist with Terraform/learn-terraform-spotify/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    spotify = {
+      version = "~> 0.2.6"
+      source  = "conradludgate/spotify"
+    }
+  }
+}
+
+provider "spotify" {
+  api_key = var.spotify_api_key
+}
+
+data "spotify_search_track" "by_artist" {
+  artist = "Kendrick Lamar"
+  #  album = "GNX"
+}
+
+resource "spotify_playlist" "playlist" {
+  name        = "Terraform Superbowl Playlist"
+  description = "This playlist was created by Terraform"
+  public      = true
+
+  tracks = [
+    data.spotify_search_track.by_artist.tracks[0].id,
+    data.spotify_search_track.by_artist.tracks[1].id,
+    data.spotify_search_track.by_artist.tracks[2].id,
+  ]
+}
